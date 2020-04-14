1f6e96376f1abe16b5c9f521ca9c11f2b2211b20
Vitaliy Kondratyuk <vitalik.deweloper@gmail.com>
2020-04-14 16:09:31 +0300

    Add migrate create_file_links_table

---
+++ database/migrations/2020_04_14_110001_create_file_links_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFileLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_links', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('file_id')->references('id')->on('user_files');
+            $table->string('alias');
+            $table->string('token')->nullable();
+            $table->boolean('single_view')->nullable();
+            $table->integer('views')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('file_links');
+    }
+}
