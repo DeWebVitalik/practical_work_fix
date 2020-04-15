4a37e45e247a8e66d04572b2298636ae7551b267
Vitaliy Kondratyuk <vitalik.deweloper@gmail.com>
2020-04-15 23:09:06 +0300

    Fix alert

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -7,7 +7,7 @@
     </div>
 @endif
 @if ($message = Session::get('error'))
-    <div class="alert alert-error">
+    <div class="alert alert-danger">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
         </button>
