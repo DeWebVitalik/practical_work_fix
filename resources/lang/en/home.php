100efccae6e8fc9a5ca64fe77240814e29254962
Vitaliy Kondratyuk <vitalik.deweloper@gmail.com>
2020-04-18 02:12:22 +0300

    Fix text

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -3,7 +3,7 @@ return [
     'title' => 'Dashboard',
     'number_link' => 'Number of link views',
     'total_file' => 'Total files',
-    'total_delete_file' => 'Total delete files',
+    'total_delete_file' => 'Total deleted files',
     'one_time_link_used' => 'Using one-time links',
     'using_one_time_link_message'=>':used of :total links has been used'
 ];
